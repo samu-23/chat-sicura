5595c5a06d9c474b1138f70f172e5a775b767f75
Tommaso Melani <melani.tommaso2@studenti-ittfedifermi.edu.it>
2024-01-27 08:45:09 +0100

    Inizializzazione progetto

---
+++ src/esercitazione_chatsicura/DiffieHellman.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package esercitazione_chatsicura;
+
+import java.io.*;
+import java.util.*;
+/**
+ *
+ * @author acer
+ */
+public class DiffieHellman {
+    
+    private File primeNumbers;
+    private List<String> primeNumbersList;
+    private int chiaveA;
+    private int chiaveB;
+    
+    public DiffieHellman(File primeNumbersFile) {
+        primeNumbers = primeNumbersFile;
+        primeNumbersList = loadFile();
+    }
+    
+    private List<String> loadFile() {
+        List<String> lines = new ArrayList<>();
+        int lineNow = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(primeNumbers.getAbsolutePath()))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                lineNow++;
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Couldn't load line " + lineNow +" into List<String>:\n" + e.getMessage());
+        }
+
+        return lines;
+    }
+    
+    public void initializeValues() {
+        
+        Random randomManager = new Random();
+        
+        int randomPrimeNumber = randomManager.nextInt(0, primeNumbersList.size());
+        int primeNumber = Integer.parseInt(primeNumbersList.get(randomPrimeNumber));
+        
+        int randomGenerator = randomManager.nextInt(2, randomPrimeNumber - 1);
+        int a = randomManager.nextInt(2, randomPrimeNumber - 1);
+        
+        //int funzA = Math.pow(randomGenerator, a) % primeNumber;
+
+        
+    }
+    
+    public int getChiaveA() {
+        return chiaveA;
+    }
+    
+    public int getChiaveB() {
+        return chiaveB;
+    }
+    
+}
---
+++ src/esercitazione_chatsicura/Esercitazione_ChatSicura.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package esercitazione_chatsicura;
+
+
+/**
+ *
+ * @author acer
+ */
+public class Esercitazione_ChatSicura {
+
+    /*private static String getCurrentProjectPath() {
+        // Ottieni il progetto corrente dal Lookup
+        Project currentProject = Lookup.getDefault().lookup(Project.class);
+
+        if (currentProject != null) {
+            // Ottieni il percorso del progetto
+            String projectPath = currentProject.getProjectDirectory().getPath();
+            return projectPath;
+        } else {
+            System.out.println("Nessun progetto corrente trovato.");
+            return null;
+        }
+    }*/
+    // Mannaggia cristo
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
